caaf77c61d9a1a233a520281be5d495a19da55bc
BeomJun-Kim <wka8963@naver.com>
2022-12-14 12:29:26 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
+console.log("test start");
 console.log(process.env["GITHUB_REF_NAME"]);
-
+console.log(process.env["POSTMAN_COLLECTION_ID"]);
+console.log(process.env["POSTMAN_API_KEY"]);
+console.log("test end");
